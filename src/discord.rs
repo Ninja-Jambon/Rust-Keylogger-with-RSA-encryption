191ec158b484404ec0c77c8b10c832bea58e54d3
Lukian LEIZOUR <lulul7018@gmail.com>
2024-01-31 16:09:43 +0100

    added discord class

--- src/discord.rs
+++ src/discord.rs
@@ -2,22 +2,22 @@ use reqwest::blocking::Client;
 use std::collections::HashMap;
 
 pub struct Discord {
-    url: &str
+    url: String
 }
 
 impl Discord {
     pub fn new(url: &str) -> Discord {
         Discord {
-            url: String::from(url),
+            url: url.to_string(),
         }
     }
 
-    pub fn send_webhook(&self, webhook_url: &str, content: &str) {
+    pub fn send_webhook(&self, content: &str) {
         let client: Client = Client::new();
         let mut map = HashMap::new();
 
         map.insert("content", content);
 
-        let _res = client.post(webhook_url).json(&map).send();
+        let _res = client.post(&self.url).json(&map).send();
     }
 }
