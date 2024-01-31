99c96a087609c1174c2f4275fd12d20674c7f3ea
Ninja-Jambon <lulul7018@gmail.com>
2024-01-31 15:39:51 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use reqwest::blocking::Client;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let client: Client = Client::new();
+
+    let url: &str = "https://discord.com/api/webhooks/1202236831631159366/T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq";
+    let mut map = HashMap::new();
+
+    map.insert("content", "caca");
+
+    let _res = client.post(url).json(&map).send();
 }
