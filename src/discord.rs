4dbe35191807cab016c91fdf5ab35072c72356bf
Ninja-Jambon <lulul7018@gmail.com>
2024-02-07 14:39:14 +0100

    commit

--- src/discord.rs
+++ src/discord.rs
@@ -2,13 +2,15 @@ use reqwest::blocking::Client;
 use std::collections::HashMap;
 
 pub struct Discord {
-    url: String
+    channel_id: String,
+    token: String,
 }
 
 impl Discord {
-    pub fn new(url: &str) -> Discord {
+    pub fn new(channel_id: &str, token: &str) -> Discord {
         Discord {
-            url: url.to_string(),
+            channel_id: channel_id.to_string(),
+            token: token.to_string(),
         }
     }
 
@@ -18,6 +20,6 @@ impl Discord {
 
         map.insert("content", content);
 
-        let _res = client.post(&self.url).json(&map).send();
+        let _res = client.post(format!("https://discord.com/api/webhooks/{}/{}", self.channel_id, self.token)).json(&map).send();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod discord;
 use discord::Discord;
 
 fn main() {
-    let discord_client: Discord = Discord::new("https://discord.com/api/webhooks/1202236831631159366/T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq");
+    // "https://discord.com/api/webhooks/1202236831631159366/T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq"
+    let discord_client: Discord = Discord::new("1202236831631159366", "T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq");
 
     discord_client.send_webhook("La belle chaise");
 }
