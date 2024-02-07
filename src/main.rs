53eb5f985dc762ff7d5189fffee78ef1ee5805e7
Ninja-Jambon <lulul7018@gmail.com>
2024-02-07 16:10:35 +0100

    added a config file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod discord;
 use discord::Discord;
+use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    channel_id: String,
+    token: String,
+}
 
 fn main() {
-    // "https://discord.com/api/webhooks/1202236831631159366/T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq"
-    let discord_client: Discord = Discord::new("1202236831631159366", "T-FQCbWFM5Kkw4i1clL9BX772E0EmJ5EyL4aSBzRuJPodZxJ_xOL2s7KpHsogFk3awzq");
+    let file_content: String = fs::read_to_string("./src/config.json").expect("Couldn't read file content.");
+    let file_content_as_str: &str = file_content.as_str();
+    let config: Config = serde_json::from_str(file_content_as_str).expect("Couldn't parse JSON.");
 
-    discord_client.send_webhook("La belle chaise");
+    let discord_client: Discord = Discord::new(config.channel_id.as_str(), config.token.as_str());
+
+    discord_client.send_webhook("Hello, Rust !");
 }
