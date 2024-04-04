baee7fba390e6d72b86e5a7c18d7d4707ae960fb
Lukian LEIZOUR <lulul7018@gmail.com>
2024-04-04 12:06:05 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use std::fs::File;
 use std::io::{self, Read};
 use std::mem;
 
-// Structure pour représenter un événement d'entrée
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 struct Timeval {
-    tv_sec: libc::c_long,    // seconds
-    tv_usec: libc::c_long,   // microseconds
+    tv_sec: libc::c_long,
+    tv_usec: libc::c_long,
 }
 
 #[repr(C)]
@@ -20,19 +19,14 @@ struct InputEvent {
 }
 
 fn main() -> io::Result<()> {
-    // Ouvre le périphérique d'entrée
     let mut file = File::open("/dev/input/event2").expect("eeror opening file");
 
-    // Boucle pour lire en continu les événements du périphérique
     loop {
-        // Lit les données d'un événement
         let mut event_data = [0u8; mem::size_of::<InputEvent>()];
         file.read_exact(&mut event_data).expect("error reading file");
 
-        // Convertit les données en structure InputEvent
         let event: InputEvent = unsafe { *(event_data.as_ptr() as *const InputEvent) };
 
-        // Affiche l'événement
         println!("{:?}", event);
     }
 }
